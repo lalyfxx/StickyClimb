cd1526628142f1ced68bc6f67a9923ae9dcc7807
Johanne-Wi <johannewidholm@gmail.com>
2026-03-12 18:28:57 +0100

    MainMenu_cod

--- ClimbingSimulator/Assets/Script_jojo/MainMenu.cs
+++ ClimbingSimulator/Assets/Script_jojo/MainMenu.cs
@@ -7,6 +7,11 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
-
+    public void QuitGame ()
+    { 
+        Debug.Log("Quit!");
+        Application.Quit();
+      
+    }
 
 }
